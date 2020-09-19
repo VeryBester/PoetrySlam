0b677520d7e6a650eecc3b0f477437e5114f077c
root <root@DESKTOP-ACLQ8HG.localdomain>
2020-09-18 23:45:17 -0500

    Started on level editor

---
+++ Assets/Scripts/LevelEditor/BeatMap.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/BeatMap", order = 1)]
+public class BeatMap : ScriptableObject
+{
+    private ArrayList Beats = new ArrayList();
+    public AudioClip Song;
+
+    private BeatComparer beatComparer = new BeatComparer();
+
+
+    private class BeatComparer : IComparer
+    {
+        int IComparer.Compare(object x, object y)
+        {
+            ArrayList a = (ArrayList)x;
+            ArrayList b = (ArrayList)y;
+            if((float)a[0] > (float)b[0]){
+                return 1;
+            } else if((float)a[0] < (float)b[0]) {
+                return -1;
+            } else{
+                return 0;
+            }
+        }
+    }    
+    public void AddBeat(ArrayList note) 
+    {
+        Beats.Add(note);
+        Beats.Sort(beatComparer);
+    }
+
+    // Unity stupid and doesnt support tuples
+    // Arraylist is used in place of Tuple<float, int>
+    public Queue<ArrayList> GetBeats() 
+    {
+        Queue<ArrayList> beats = new Queue<ArrayList>();
+        foreach (ArrayList beat in Beats)
+        {
+            beats.Enqueue(beat);
+        }
+        return beats;
+    }
+
+}
---
+++ Assets/Scripts/LevelEditor/LevelEditor.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelEditor : MonoBehaviour
+{
+    // Drag in what beatmap you want to update through unity editor
+    public BeatMap beatMap;
+    private AudioSource audioSource;
+
+    private bool paused = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = beatMap.Song;
+        PlayMusic();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            if(paused) { 
+                PlayMusic();
+            } else {
+                PauseMusic();
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            ArrayList note = new ArrayList();
+            note.Add(audioSource.time);
+            note.Add(1);
+            beatMap.AddBeat(note);
+        }
+    }
+
+    private void PlayMusic()
+    {
+        paused = false;
+        audioSource.Play();
+    }
+
+    private void PauseMusic() 
+    {
+        paused = true;
+        audioSource.Pause();
+    }
+}
