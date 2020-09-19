d77ce85848bbf5dc29ab9e9d30d75609141cb205
root <root@DESKTOP-ACLQ8HG.localdomain>
2020-09-19 14:57:13 -0500

    Created constant file

---
+++ Assets/Scripts/GameConstants.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameConstants
+{
+    KeyCode botButton1 = KeyCode.D;
+    KeyCode botButton2 = KeyCode.F;
+
+    KeyCode topButton1 = KeyCode.J;
+    KeyCode topButton2 = KeyCode.K;
+}
