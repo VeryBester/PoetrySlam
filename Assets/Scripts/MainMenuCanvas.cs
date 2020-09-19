802711910f23dbcc7c9dcd4647a0d191969db665
Hamza <hamzaimran2011@gmail.com>
2020-09-18 22:51:01 -0500

    Started on Main Menu UI
    Made a different scene to hold all my changes, so it shouldn't cause any issues. There is a bool named 'toggle' in the canvas you can toggle to see the ui when running the game

---
+++ Assets/Scripts/MainMenuCanvas.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuCanvas : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject CreditsMenu;
+    public GameObject MainMenuBackground;
+
+    public bool toggle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!toggle) {
+            MainMenu.SetActive(false);
+            CreditsMenu.SetActive(false);
+            MainMenuBackground.SetActive(false);
+
+        } else {
+            MainMenuButton();
+        }
+    }
+
+    public void PlayNowButton()
+    {
+        // switch to game scene
+        UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
+    }
+
+    public void CreditsButton()
+    {
+        // Show Credits Menu
+        MainMenu.SetActive(false);
+        CreditsMenu.SetActive(true);
+    }
+
+    public void MainMenuButton()
+    {
+        // Show Main Menu
+        MainMenu.SetActive(true);
+        CreditsMenu.SetActive(false);
+    }
+
+    public void QuitButton()
+    {
+        // Quit Game
+        Application.Quit();
+    }
+}
